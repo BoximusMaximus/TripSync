7fdf7f51a7c18e363b7dabfc2f1225d8d79fdd0d
gadm12 <gadm5336@gmail.com>
2026-09-01 20:27:49 -0500

    navbar complete

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -7,14 +8,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignUpPage />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+    ],
   },
 ]);
 
