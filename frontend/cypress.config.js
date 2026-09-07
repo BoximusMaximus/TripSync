02a5436b44416aa2b74c0aad4bc7e7d11355a7cd
gadm12 <gadm5336@gmail.com>
2026-09-07 17:01:14 -0500

    update cypress.config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-    e2e: {
-        baseUrl: "http://localhost:5173/",
-        supportFile: false,
-    },
-    viewportWidth:1024,
-    viewportHeight:768,
-    video:false,
-})
+  e2e: {
+    baseUrl: "http://localhost:5173/",
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 20000,
+    supportFile: false,
+  },
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  video: false,
+});
